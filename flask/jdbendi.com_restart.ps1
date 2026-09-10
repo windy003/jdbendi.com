73ede1e593e0e2779a9ed1326cb18541e83569a1
windy003 <928920471@qq.com>
2026-09-10 08:36:14 +0800

    在主页显示仅自己可见的badge

---
+++ flask/jdbendi.com_restart.ps1
@@ -0,0 +1,2 @@
+./jdbendi.com_close.ps1
+./jdbendi.com_start.ps1
---
+++ flask/jdbendi.com_start.ps1
@@ -0,0 +1,2 @@
+Set-Location "D:\files\using\Web\jdbendi.com\flask"
+& "D:\files\using\Web\jdbendi.com\flask\.venv\Scripts\pythonw.exe" "D:\files\using\Web\jdbendi.com\flask\app.py"
